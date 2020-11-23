b20a4fd7a89a3685aadff8a05ec2bfc962f01e3c
eze-kiel <ezekiel@fastmail.com>
2020-11-23 21:28:37 +0100

    typo

--- cmd/serve.go
+++ cmd/serve.go
@@ -27,8 +27,8 @@ This will serve the file test.txt on 127.0.0.1:8080
 This will serve blah.txt on 192.168.1.36:1337:
   shaloc serve -f blah.txt -i 192.168.1.36 -p 1337
 
-This will serve the folder /home/ezekiel/s3cr3t-f1l3s on 127.0.0.1:8080:
-  shaloc server -F /home/ezekiel/s3cr3t-f1l3s
+This will serve the folder /home/user/sup3r-f0ld3r on 127.0.0.1:8080:
+  shaloc server -F /home/user/sup3r-f0ld3r
 `,
 
 	Run: func(cmd *cobra.Command, args []string) {
