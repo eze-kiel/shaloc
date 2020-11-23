b21aa7298be2ad3d7356981a1b5fc4f3ee5452fd
eze-kiel <ezekiel@fastmail.com>
2020-11-23 18:08:09 +0100

    first commit

---
+++ cmd/get.go
@@ -0,0 +1,92 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/briandowns/spinner"
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Download a file from an URL",
+	Long: `You can download a file from an URL. For example:
+
+This will create a file called 'out':
+  shaloc get -u http://192.168.1.133/file.txt
+
+This will create a file called new.txt:
+  shaloc get -u http://192.168.1.133/file.txt -n new.txt
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		url, _ := cmd.Flags().GetString("url")
+		name, _ := cmd.Flags().GetString("name")
+
+		if url == "" {
+			fmt.Println("You must provide a URL with the flag -u !")
+			os.Exit(1)
+		}
+
+		err := download(name, url)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Downloaded: " + url)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+	getCmd.Flags().StringP("url", "u", "", "URL to download the file from.")
+	getCmd.Flags().StringP("name", "n", "out", "Name of the file that will be downloaded.")
+}
+
+func download(filepath string, url string) error {
+
+	// Init and start the spinner
+	s := spinner.New(spinner.CharSets[23], 100*time.Millisecond)
+	s.Start()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+
+	// Stop the spinner
+	s.Stop()
+
+	return err
+}
---
+++ cmd/serve.go
@@ -0,0 +1,78 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "serve a file",
+	Long: `serve allow you to start a HTTP server to serve a file. For example:
+
+This will serve the file test.txt on 127.0.0.1:8080
+  shaloc serve -f test.txt
+
+This will serve blah.txt on 192.168.1.36:1337:
+  shaloc serve -f blah.txt -i 192.168.1.36 -p 1337`,
+	Run: func(cmd *cobra.Command, args []string) {
+		ip, _ := cmd.Flags().GetString("ip")
+		port, _ := cmd.Flags().GetString("port")
+		file, _ := cmd.Flags().GetString("file")
+
+		if file == "" {
+			fmt.Println("You must provide a file to share with the flag -f !")
+			os.Exit(1)
+		}
+
+		http.HandleFunc("/"+file, func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Disposition", "attachment; filename="+file)
+			w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+			w.Header().Set("Content-Length", r.Header.Get("Content-Length"))
+
+			Openfile, err := os.Open(file)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			io.Copy(w, Openfile)
+		})
+
+		fmt.Printf("Now serving on http://%s:%s/%s\n", ip, port, file)
+		srv := &http.Server{Addr: ip + ":" + port}
+
+		// Handles networking errors, such as being unable to bind IP or port
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("HTTP Server: ListenAndServe() error: %s", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+	serveCmd.Flags().StringP("ip", "i", "127.0.0.1", "IP address to serve on.")
+	serveCmd.Flags().StringP("port", "p", "8080", "Port to serve on.")
+	serveCmd.Flags().StringP("file", "f", "", "File to share.")
+}
