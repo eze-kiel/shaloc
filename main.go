1e2252ddd027a87863a92fac78cc890a0248d95e
eze-kiel <ezekiel@fastmail.com>
2020-11-25 10:18:30 +0100

    updated LDFLAGS

--- main.go
+++ main.go
@@ -2,13 +2,6 @@ package main
 
 import "github.com/eze-kiel/shaloc/cmd"
 
-var (
-	// Version holds the build version
-	Version string
-	// BuildDate holds the build date
-	BuildDate string
-)
-
 func main() {
 	cmd.Execute()
 }
